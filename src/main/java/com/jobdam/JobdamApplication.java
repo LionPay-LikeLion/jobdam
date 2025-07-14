97cd229ad95ddb0b01d5080bd519ea8b5a2f607f
juanpark <juanpark@me.com>
2025-07-14 15:05:24 +0900

    Spring Boot initial set up

---
+++ src/main/java/com/jobdam/JobdamApplication.java
@@ -0,0 +1,13 @@
+package com.jobdam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobdamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobdamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobdam/JobdamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobdam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobdamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
