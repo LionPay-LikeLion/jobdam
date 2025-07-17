e686fe0726d03352fadef8c23ed623ee02872a6b
sunhyun0508 <kko2647@naver.com>
2025-07-17 14:53:29 +0900

    프리미엄. 일반에 따른 북마크,게시글, 피드 노출 제한 기능

--- src/main/java/com/jobdam/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jobdam/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.jobdam.common.exception;
 
+import io.swagger.v3.oas.annotations.Hidden;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
-
+@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
--- src/main/java/com/jobdam/common/exception/LimitExceededException.java
+++ src/main/java/com/jobdam/common/exception/LimitExceededException.java
@@ -3,7 +3,7 @@ package com.jobdam.common.exception;
 public class LimitExceededException extends RuntimeException {
 
     public LimitExceededException(String message) {
-        super(message);
+        super(message != null ? message : "제한을 초과했습니다.");
     }
 
 }
